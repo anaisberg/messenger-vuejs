ef7a6c691954df2108c85eb5fac5640d82ee3c7e
Anais <anais.schlienger@mines-paristech.fr>
2020-02-18 13:32:09 +0100

    ajout de l'auteur dans l'affichage

--- vuejs/vue.js
+++ vuejs/vue.js
@@ -1,16 +1,13 @@
 const Msg = Vue.component('msg', {
-    props: ['author', 'message'],
+    props: ['author', 'message', 'hour'],
     template: '<div class="msg"><h4><em>{{ author }}</em></h4><p>{{ message }}</p></div>'
 });
 
 const app = new Vue({
     el: '#app',
     data: {
-        newText: [],
-        newAuthor: '',
-        newTime: '',
-        newMessage: '',
-        newTexts: [],
+        newAuthor: [],
+        newMessage: [],
         messages: [],
     },
     created: function() {
@@ -20,16 +17,30 @@ const app = new Vue({
             const data = await res.json();
             self.messages = [];
             data.forEach(function (r, i) {
-                self.messages.push(['Theo',r.content]);
+                self.messages.push(this.receiveMsg(r.content,r.ts));
             });
         }, 300);
     },
     computed: {
+        newMsg: function() {
+            return newAuthor+'@'+newMessage
+        },
+
     },
     methods: {
         post: function(){
-          fetch('/test/post/'+this.newMessage);
+          fetch('/test/post/'+this.newMsg);
+        },
+        receiveMsg: function(mes,time) {
+            var n = mes.length;
+            for (let i=0; i < n; i++) {
+                if (mes[i].equals('@')) {
+                    return [mes.substring(0,i),mes.substring(i+1,n),time]
+                }
+            }
+            return ['none',me,time]
         }
+
     }
 });
 
